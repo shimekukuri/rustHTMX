3faed9e0e4ad04e4215ea562d95eeaa438d21361
Tyler Hutchinson <jhutchinson192390@gmail.com>
2024-05-10 16:41:14 -0400

    adding stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("world");
+    format!("hello {}", name)
+}
+
+async fn health_check(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/api", web::get().to(health_check))
+            .route("api/{name}", web::get().to(greet))
+    })
+    .bind("0.0.0.0:4000")?
+    .run()
+    .await
 }
